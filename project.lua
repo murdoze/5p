451ee858efde7d0ef285882ac1b16f19e12482d0
Roman Pavlyuk <roman@luftronix.com>
2022-01-07 22:53:19 +0200

    Main project file

---
+++ project.lua
@@ -0,0 +1,139 @@
+local pp, inkey, push = unpack(require 'lib/all')
+
+local hl = require'highlight'
+local genid = require'genid'
+
+require'noglobals'
+
+-- Data Model
+
+local Item = 
+{
+  Person = 'PERSON',
+  Task = 'TASK',
+  Customer = 'CUSTOMER',
+  Milestone = 'MILESTONE',
+  Drone = 'DRONE',
+
+}
+
+local People = {}
+local Tasks = {}
+
+-- Data Base
+
+local p, t, c, m, d
+
+p =
+{
+  id = genid(),
+  item = Item.Person,
+  name = 'RP',
+  decription = 'Roman Pavlyuk, CTO, характєр мєрзкій, нє женат'
+}
+
+
+t =
+{
+    id = genid()
+}
+
+
+
+-- Chord processing
+
+local current_chord = ''
+
+local chords = {}
+
+local function makeChord(chord, func, description, continue_chord)
+  chords[chord] = { func =  func, description = description, continue = continue_chord }
+end
+
+local function charsFor(key)
+  local keymap=
+  {
+  }
+
+  local cmd = keymap[key]
+  if cmd == nil then
+    if key >= 0 and key <= 255 then
+      cmd = string.char(key)
+    else
+      cmd = ''
+    end
+  end
+
+  return cmd
+end
+
+local function chordFor(key)
+  local chars = charsFor(key)
+  current_chord = current_chord .. chars
+
+  print("==>" .. current_chord .. "<==")
+
+  local chord = chords[current_chord]
+
+  if chord ~= nil then
+    -- play the chord
+
+    if chord.continue then
+    else
+      current_chord = ''
+    end
+
+    return chord
+  else
+    -- start over
+    current_chord = ''
+
+    return nil
+  end
+end
+
+-- Chords
+
+local function showHelp()
+  print(hl.Cls())
+  print(hl.Locate(15, 50)() .. "HELP !!!!")
+  print('HELP !!!!!!!')
+end
+
+
+makeChord('?', showHelp, 'Show chords help')
+
+
+
+do
+  local key = 0
+  local chars = ''
+  local chord = nil
+
+  while chars ~= 'q' do
+ 
+    key = inkey()
+    print(hl.Cls())
+ 
+    chars = charsFor(key)
+    chord = chordFor(key)
+
+   
+
+    print(hl.Locate(10, 40)() .. key .. " => [" .. current_chord .. "] " .. genid())
+
+    if chord ~= nil then
+      local func = chord.func
+      if func ~= nil then
+        func()
+        pp(chord)
+      end
+    end  
+
+  end
+
+  hl.printColorTable()
+
+end
+
+
