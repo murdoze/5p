96a594cb02a2bca30887a88770b68ad9923c7382
Roman Pavlyuk <roman@luftronix.com>
2022-01-08 14:16:55 +0200

    Added string.cut function

--- lib/all.lua
+++ lib/all.lua
@@ -1,6 +1,8 @@
 -- Loads common functions and modules, returns them as a list
 -- Beware of order!
 
+-- Paths and registration
+
 package.cpath='./ext/?/?.so;'..package.cpath
 package.path='./lib/?.lua;'..package.path
 
@@ -12,7 +14,7 @@ local function register(...)
   end
 end
 
---
+-- String utilities
 
 string.pad = function(s, width, char)
   local pad = width - string.len(s)
@@ -24,7 +26,15 @@ string.pad = function(s, width, char)
   end
 end
 
---
+string.cut = function(s, width)
+  if string.len(s) > width then
+    return string.sub(s, 1, width)
+  else
+    return s
+  end
+end
+
+-- Exporting common functions
 
 local serpent = require'serpent'
 local pp = function(_) print(serpent.block(_, {nocode = true, sortkeys = true, comment = false})) end
