eef37edeee053aa5f3746889405ed054ca200771
Roman Pavlyuk <roman@luftronix.com>
2022-01-09 23:23:04 +0200

    Data model

---
+++ data.lua
@@ -0,0 +1,78 @@
+-- Data Model
+local genid = require'genid'
+
+local data = {}
+
+local Item = 
+{
+  Person = 'PERSON',
+  Task = 'TASK',
+  Customer = 'CUSTOMER',
+  Milestone = 'MILESTONE',
+  Drone = 'DRONE',
+
+}
+
+local people = {}
+local tasks = {}
+
+data.Item = Item
+data.people = people
+data.tasks = tasks
+
+
+-- Data Base
+
+local p, t, c, m, d
+
+--- People
+
+p =
+{
+  id = genid(),
+  item = Item.Person,
+  name = 'RP',
+  -- text = 'Roman Pavlyuk, CTO, характєр мєрзкій, нє женат'
+  text = 'Roman Pavlyuk, CTO, a very inteligent and self-loving human being, a dream of every woman'
+}
+table.insert(people, p)
+
+p =
+{
+  id = genid(),
+  item = Item.Person,
+  name = 'AL',
+  text = 'Anton Lutsyshyn, let the moon shine and let the circuits blow!'
+}
+table.insert(people, p)
+
+
+--- Tasks
+
+t =
+{
+  id = genid(),
+  item = Item.Task,
+  text = 'Gimbal',
+  assigned_to = p,
+}
+table.insert(tasks, t)
+
+  for i=1,10000 do
+  t =
+  {
+    id = genid(),
+    item = Item.Task,
+    text = 'Task #' .. tostring(i+1) .. ": this is a very important task, a very long line, and overall life is suffering and we all shall eventually die",
+    assigned_to = p,
+  }
+  table.insert(tasks, t)
+end
+
+
+-- local t_ser = serpent.dump(t)
+-- pp"======= TASK ==================================="
+-- print(t_ser)
+-- pp"================================================"
+
+return data
