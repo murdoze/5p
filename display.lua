c4d5bb16c0b4dabec9ad9baccb45814d596930ad
Roman Pavlyuk <roman@luftronix.com>
2022-01-09 23:24:56 +0200

    Terminal window resizing and border drawing

--- display.lua
+++ display.lua
@@ -7,21 +7,36 @@ local display = {}
 display.width = 80
 display.height = 30
 
-display.header_line = 3
+display.header_line = 2
 display.title_line = display.header_line + 2
 display.status_line = -1
 
 display.list_begin_line = display.title_line + 3
 display.list_end_line = -1
 
-display.on_resize = function(height, width)
-  display.status_line = height
+display.get_window_size = function()
+  local tmp_lines=os.tmpname()
+  local tmp_cols=os.tmpname()
+  os.execute("tput lines > " .. tmp_lines)
+  os.execute("tput cols > " .. tmp_cols)
+  local h = tonumber(io.lines(tmp_lines)())
+  local w = tonumber(io.lines(tmp_cols)())
+  os.remove(tmp_lines)
+  os.remove(tmp_cols)
+
+  display.height = h
+  display.width = w
+end
+
+display.handle_resize = function()
+  display.status_line = display.height - 1
   display.list_end_line = display.status_line - 2
 end
 
 --
+
 display.locate = function(line, column)
-  if column == nil then column = 1 end
+  if column == nil then column = 2 end
   io.write(hl.Locate(line, column)())
 end
 
@@ -30,20 +45,26 @@ display.print = function(s)
 end
 
 display.print_line = function(s)
-  io.write(hl.align(s, display.width) .. "|" .. hl.Off())
+  io.write(hl.align(s, display.width - 2) .. hl.Off())
 end
 
-display.show_header = function() end
-display.show_status = function() end
-display.show_keys = function() end
+local noop = function() end
+
+display.show_header = noop
+display.show_status = noop
+display.show_keys = noop
+display.show_border = noop
 
 display.current_screen = function() end
 
 display.draw = function()
-  display.on_resize(display.height, display.width)
   io.flush()
-  print(hl.Cls())
 
+  display.get_window_size()
+  display.handle_resize()
+  display.print(hl.Cls())
+
+  display.show_border()
   display.show_header()
   display.current_screen()
   display.show_status()
