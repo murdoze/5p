964ed24c91a5010ff5d8c3efbf59a2c8f6bf69e3
Roman Pavlyuk <roman@luftronix.com>
2022-01-14 00:42:58 +0400

    Added readline via Luajit FFI

--- lib/all.lua
+++ lib/all.lua
@@ -37,15 +37,40 @@ end
 -- Exporting common functions
 
 local serpent = require'serpent'
-local pp = function(_) print(serpent.block(_, {nocode = true, sortkeys = true, comment = false})) end
+local pp = function(_) print(serpent.block(_, { nocode = true, sortkeys = true, comment = false })) end
 
 local kb = require'kb'
 local getch = function() return kb.getch() end
 
 local push = function(t, v) table.insert(t, v) end
 
+local ffi = require'ffi'
+ffi.cdef[[
+  /* libc definitions */
+  void* malloc(size_t bytes);
+  void free(void *);
+  
+  /* basic history handling */
+  char *readline (const char *prompt);
+  void add_history(const char *line);
+  
+  /* completion */
+  typedef char **rl_completion_func_t (const char *, int, int);
+  typedef char *rl_compentry_func_t (const char *, int);
+  
+  char **rl_completion_matches (const char *, rl_compentry_func_t *);
+  
+  const char *rl_basic_word_break_characters;
+  rl_completion_func_t *rl_attempted_completion_function;
+  char *rl_line_buffer;
+  int rl_completion_append_character;
+  int rl_attempted_completion_over;
+]]
+local libreadline = ffi.load("readline")
+local read = function() libreadline.readline("> ") end
+
 register(
-  pp, getch, push
+  pp, getch, push, read
 )
 
 return all
